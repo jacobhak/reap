ae00207314dbc43ca97508ecf4f8ac7637316f4b
Jacob Håkansson <jacobhakansson@gmail.com>
2016-06-17 21:28:02 +0200

    Added configuration

---
+++ config.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+const tar = require('keytar');
+const fs = require('mz/fs');
+const path = require('path');
+
+const dir = path.join(process.env.HOME, '.reap');
+const file = path.join(dir, 'config.json');
+
+const promptForConfig = () => {
+  const questions = [
+    {
+      'type': 'input',
+      'name': 'subdomain',
+      'message': 'subdomain: '
+    },
+    {
+      'type': 'input',
+      'name': 'username',
+      'message': 'username: '
+    },
+    {
+      'type': 'password',
+      'name': 'password',
+      'message': 'password: '
+    }
+  ];
+
+  return inquirer.prompt(questions);
+};
+
+const writeConfig = (answers) => {
+  console.log(`writing ${JSON.stringify(answers)}`);
+  const write = () => {
+    return fs.writeFile(file, JSON.stringify({
+      'subdomain': answers.subdomain,
+      'username': answers.username
+    }, 2)).then(() => {
+      tar.replacePassword('reap-harvest', answers.username, answers.password);
+      return answers;
+    });
+  };
+
+  if (!fs.existsSync(dir)) {
+    return fs.mkdir(dir).then(write);
+  } else {
+    return write();
+  }
+};
+
+const readConfig = () => {
+  return fs.readFile(file)
+    .then(contents => {
+      let c = JSON.parse(contents);
+      return new Promise((res, rej) => {
+        const pass = tar.getPassword('reap-harvest', c.username);
+        if (!pass) {
+          rej();
+        } else {
+          c.password = pass;
+          res(c);
+        }
+      });
+    })
+    .catch(() => {
+      console.log('No config found');
+      return promptForConfig().then(writeConfig);
+    });
+};
+
+module.exports = readConfig();
